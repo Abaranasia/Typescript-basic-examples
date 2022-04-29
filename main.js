988df4911d9056f1e002c72b920b534fd755282d
Ran Kirlian <rankirlian@gmail.com>
2022-04-29 20:25:47 +0200

    Add namespaces example

--- main.js
+++ main.js
@@ -1,93 +1,16 @@
 "use strict";
-(() => {
-    let batman = {
-        name: 'Bruce Wayne',
-        age: 45,
-        powers: ['none'],
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3)
+            ? true
+            : false;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 60,
-        powers: ['super speed', 'flight', 'super power'],
+    const validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf()))
+            ? false
+            : true;
     };
-})();
-(() => {
-    ;
-    class Mutant {
-        constructor(id, age, name, realName) {
-            this.id = id;
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-            this.id = id;
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        fullName(id) {
-            return this.name + ' is: ' + this.realName;
-        }
-        ;
-    }
-})();
-(() => {
-    ;
-    ;
-    const client = {
-        name: 'Pepe',
-        age: 35,
-        address: {
-            id: 125,
-            zip: 'KY2 SUD',
-            city: 'Ottawa'
-        },
-        getFulAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    ;
-    let addTwoNumbersFunction;
-    addTwoNumbersFunction = (a, b) => {
-        return a + b;
-    };
-})();
-const conducirBatimovil = (auto) => {
-    auto.encender = true;
-    auto.velocidadMaxima = 100;
-    auto.acelerar();
-};
-const batimovil = {
-    encender: false,
-    velocidadMaxima: 0,
-    acelerar() {
-        console.log("...... gogogo!!!");
-    }
-};
-;
-const guason = {
-    reir: true,
-    comer: true,
-    llorar: false
-};
-const reir = (guason) => {
-    if (guason.reir) {
-        console.log("JAJAJAJA");
-    }
-};
-;
-const ciudadGotica = (ciudadanos) => {
-    return ciudadanos.length;
-};
-class Persona {
-    constructor() {
-        this.nombre = "";
-        this.edad = 0;
-        this.sexo = "";
-        this.estadoCivil = ";";
-    }
-    imprimirBio() {
-    }
-}
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('pep'));
 //# sourceMappingURL=main.js.map
